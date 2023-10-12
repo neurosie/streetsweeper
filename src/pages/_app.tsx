563c6c3454760c5d218d03c74d46711d67c93f1d
Hayes Neuman <neurosie@gmail.com>
2023-10-12 17:07:16 -0400

    Visual design of the landing page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import { type AppType } from "next/app";
+import { Overpass } from "next/font/google";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
+const overpass = Overpass({
+  subsets: ["latin"],
+  variable: "--font-overpass",
+});
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${overpass.variable} bg-sign-400 min-h-screen font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default api.withTRPC(MyApp);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,18 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sign: {
+          200: "#DFF0EA",
+          400: "#7FC9B2",
+          800: "#006747",
+        },
+      },
+      fontFamily: {
+        sans: ["var(--font-overpass)"],
+      },
+    },
   },
   plugins: [require("@headlessui/tailwindcss")],
 } satisfies Config;
